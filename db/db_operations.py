09c568e4fedc82634b784029b03311d9f203be6c
Gustavo <Gustavo.Chavez10118@gmail.com>
2025-07-01 22:16:13 -0400

    Added database and CRUD file

---
+++ db/db_operations.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+'''
+    Functions for the budgets table (these will be sql querys)
+'''
+# list all the contents of the table
+def get_all_budgets():
+    pass
+
+# add a query to a table
+def add_to_budget():
+    pass
+
+# delete a query from the table
+def delete_from_budget():
+    pass
+
+'''
+    Functions for the expenses table
+'''
+
+# list all the expenses
+def get_all_expenses():
+    pass
+
+# add to expenses
+def add_expense():
+    pass
+
+# delete from expense
+def delete_expense():
+    pass
